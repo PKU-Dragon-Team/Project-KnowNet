985c8b41eba1a61b9900b9748cb1b37692debe70
SLAPaper <slapaper.pku@gmail.com>
2018-11-07 03:29:06 +0800

    test: update test

---
+++ test/test_data_platform.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import unittest as ut
+from pathlib import Path
+
+root_folder = Path(os.getcwd())
+sys.path.append(str(root_folder))
+
+
+class TestJSONDS(ut.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.jsds = None
+
+    def setUp(self):
+        pass
+
+    def test_import(self):
+        import data_platform.datasource
+
+        self.jsds = data_platform.datasource.JSONDS
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    ut.main()
